14eb80102a6d7a8adff376386bd577b32d5cffc8
Tien Le <tien.le@fruitful.io>
2021-07-19 13:22:46 +0700

    Revert "add onKeywordChanged"
    This reverts commit 75a5b4e3616069068a14992de31d0fe399c81d76.

--- src/components/mention-input.tsx
+++ src/components/mention-input.tsx
@@ -1,20 +1,13 @@
-import React, {
-  FC,
-  MutableRefObject,
-  useMemo,
-  useRef,
-  useState,
-  useEffect,
-} from "react";
+import React, { FC, MutableRefObject, useMemo, useRef, useState } from 'react';
 import {
   NativeSyntheticEvent,
   Text,
   TextInput,
   TextInputSelectionChangeEventData,
   View,
-} from "react-native";
+} from 'react-native';
 
-import { MentionInputProps, MentionPartType, Suggestion } from "../types";
+import { MentionInputProps, MentionPartType, Suggestion } from '../types';
 import {
   defaultMentionTextStyle,
   generateValueFromPartsAndChangedText,
@@ -22,38 +15,34 @@ import {
   getMentionPartSuggestionKeywords,
   isMentionPartType,
   parseValue,
-} from "../utils";
+} from '../utils';
 
-const MentionInput: FC<MentionInputProps> = ({
-  value,
-  onChange,
+const MentionInput: FC<MentionInputProps> = (
+  {
+    value,
+    onChange,
 
-  partTypes = [],
+    partTypes = [],
 
-  inputRef: propInputRef,
+    inputRef: propInputRef,
 
-  onKeywordChanged,
+    containerStyle,
 
-  containerStyle,
+    onSelectionChange,
 
-  onSelectionChange,
-
-  ...textInputProps
-}) => {
+    ...textInputProps
+  },
+) => {
   const textInput = useRef<TextInput | null>(null);
 
-  const [selection, setSelection] = useState({ start: 0, end: 0 });
+  const [selection, setSelection] = useState({start: 0, end: 0});
 
-  const [keywordByTrigger, setKeywordByTrigger] = useState({});
+  const {
+    plainText,
+    parts,
+  } = useMemo(() => parseValue(value, partTypes), [value, partTypes]);
 
-  const { plainText, parts } = useMemo(
-    () => parseValue(value, partTypes),
-    [value, partTypes]
-  );
-
-  const handleSelectionChange = (
-    event: NativeSyntheticEvent<TextInputSelectionChangeEventData>
-  ) => {
+  const handleSelectionChange = (event: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => {
     setSelection(event.nativeEvent.selection);
 
     onSelectionChange && onSelectionChange(event);
@@ -65,24 +54,19 @@ const MentionInput: FC<MentionInputProps> = ({
    * @param changedText
    */
   const onChangeInput = (changedText: string) => {
-    onChange(
-      generateValueFromPartsAndChangedText(parts, plainText, changedText)
-    );
+    onChange(generateValueFromPartsAndChangedText(parts, plainText, changedText));
   };
 
   /**
    * We memoize the keyword to know should we show mention suggestions or not
    */
-
-  useEffect(() => {
-    const keyword = getMentionPartSuggestionKeywords(
+  const keywordByTrigger = useMemo(() => {
+    return getMentionPartSuggestionKeywords(
       parts,
       plainText,
       selection,
-      partTypes
+      partTypes,
     );
-    setKeywordByTrigger(keyword);
-    onKeywordChanged && onKeywordChanged(keyword);
   }, [parts, plainText, selection, partTypes]);
 
   /**
@@ -90,103 +74,99 @@ const MentionInput: FC<MentionInputProps> = ({
    * - Get updated value
    * - Trigger onChange callback with new value
    */
-  const onSuggestionPress =
-    (mentionType: MentionPartType) => (suggestion: Suggestion) => {
-      const newValue = generateValueWithAddedSuggestion(
-        parts,
-        mentionType,
-        plainText,
-        selection,
-        suggestion
-      );
+  const onSuggestionPress = (mentionType: MentionPartType) => (suggestion: Suggestion) => {
+    const newValue = generateValueWithAddedSuggestion(
+      parts,
+      mentionType,
+      plainText,
+      selection,
+      suggestion,
+    );
 
-      if (!newValue) {
-        return;
-      }
+    if (!newValue) {
+      return;
+    }
 
-      onChange(newValue);
+    onChange(newValue);
 
-      /**
-       * Move cursor to the end of just added mention starting from trigger string and including:
-       * - Length of trigger string
-       * - Length of mention name
-       * - Length of space after mention (1)
-       *
-       * Not working now due to the RN bug
-       */
-      // const newCursorPosition = currentPart.position.start + triggerPartIndex + trigger.length +
-      // suggestion.name.length + 1;
+    /**
+     * Move cursor to the end of just added mention starting from trigger string and including:
+     * - Length of trigger string
+     * - Length of mention name
+     * - Length of space after mention (1)
+     *
+     * Not working now due to the RN bug
+     */
+    // const newCursorPosition = currentPart.position.start + triggerPartIndex + trigger.length +
+    // suggestion.name.length + 1;
 
-      // textInput.current?.setNativeProps({selection: {start: newCursorPosition, end: newCursorPosition}});
-    };
+    // textInput.current?.setNativeProps({selection: {start: newCursorPosition, end: newCursorPosition}});
+  };
 
   const handleTextInputRef = (ref: TextInput) => {
     textInput.current = ref as TextInput;
 
     if (propInputRef) {
-      if (typeof propInputRef === "function") {
+      if (typeof propInputRef === 'function') {
         propInputRef(ref);
       } else {
-        (propInputRef as MutableRefObject<TextInput>).current =
-          ref as TextInput;
+        (propInputRef as MutableRefObject<TextInput>).current = ref as TextInput;
       }
     }
   };
 
   const renderMentionSuggestions = (mentionType: MentionPartType) => (
     <React.Fragment key={mentionType.trigger}>
-      {mentionType.renderSuggestions &&
-        mentionType.renderSuggestions({
-          keyword: keywordByTrigger[mentionType.trigger]
-            ? keywordByTrigger[mentionType.trigger]
-            : "",
-          onSuggestionPress: onSuggestionPress(mentionType),
-        })}
+      {mentionType.renderSuggestions && mentionType.renderSuggestions({
+        keyword: keywordByTrigger[mentionType.trigger],
+        onSuggestionPress: onSuggestionPress(mentionType),
+      })}
     </React.Fragment>
   );
 
   return (
     <View style={containerStyle}>
-      {(
-        partTypes.filter(
-          (one) =>
-            isMentionPartType(one) &&
-            one.renderSuggestions != null &&
-            !one.isBottomMentionSuggestionsRender
-        ) as MentionPartType[]
-      ).map(renderMentionSuggestions)}
+      {(partTypes
+        .filter(one => (
+          isMentionPartType(one)
+          && one.renderSuggestions != null
+          && !one.isBottomMentionSuggestionsRender
+        )) as MentionPartType[])
+        .map(renderMentionSuggestions)
+      }
 
       <TextInput
         multiline
+
         {...textInputProps}
+
         ref={handleTextInputRef}
+
         onChangeText={onChangeInput}
         onSelectionChange={handleSelectionChange}
       >
         <Text>
-          {parts.map(({ text, partType, data }, index) =>
-            partType ? (
-              <Text
-                key={`${index}-${data?.trigger ?? "pattern"}`}
-                style={partType.textStyle ?? defaultMentionTextStyle}
-              >
-                {text}
-              </Text>
-            ) : (
-              <Text key={index}>{text}</Text>
-            )
-          )}
+          {parts.map(({text, partType, data}, index) => partType ? (
+            <Text
+              key={`${index}-${data?.trigger ?? 'pattern'}`}
+              style={partType.textStyle ?? defaultMentionTextStyle}
+            >
+              {text}
+            </Text>
+          ) : (
+            <Text key={index}>{text}</Text>
+          ))}
         </Text>
       </TextInput>
 
-      {(
-        partTypes.filter(
-          (one) =>
-            isMentionPartType(one) &&
-            one.renderSuggestions != null &&
-            one.isBottomMentionSuggestionsRender
-        ) as MentionPartType[]
-      ).map(renderMentionSuggestions)}
+      {(partTypes
+        .filter(one => (
+          isMentionPartType(one)
+          && one.renderSuggestions != null
+          && one.isBottomMentionSuggestionsRender
+        )) as MentionPartType[])
+        .map(renderMentionSuggestions)
+      }
     </View>
   );
 };
